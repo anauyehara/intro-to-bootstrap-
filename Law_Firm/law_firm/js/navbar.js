b558133f8e5b449a4642507f4f481dd3d3acb488
Ana uyehara <ana.uyehara0@gmail.com>
2019-08-27 22:02:46 -0700

    test

--- Law_Firm/law_firm/js/navbar.js
+++ Law_Firm/law_firm/js/navbar.js
@@ -1,5 +1,12 @@
-// document.addEventListener('click', doThing);
+// varvar btn = document.getElementsByClassName('button');
+// console.log(btn [0]);
+// var btnusingselector = document.querySelector ('.button');
+// console.log(btnusingselector);
+// btn[0].style.padding = '10px';
+// btn[0].style.fontsize = '22px';
 
-// function doThing(event) {
-  
-// }
+
+body{
+ background-Image: url(conf-client-1.jpg);
+ background-position: 'center';
+}entListener('click', doThing/ }
